96ec0d9ecc1ccc88c3b9580dd415e202a93f10c4
dejanV91 <dejanvukoicic91@gmail.com>
2022-12-04 11:52:02 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,14 +8,12 @@ import Error from "./pages/Error";
 function App() {
   return (
     <BrowserRouter>
-      <nav>our navbar</nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="products" element={<Products />} />
         <Route path="*" element={<Error />} />
       </Routes>
-      <footer>our footer</footer>
     </BrowserRouter>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <section className="section">
-      <h2>Home Page</h2>
-      <Link to={"/about"} className="btn">
-        About
-      </Link>
+      <h2>title</h2>
+      <Outlet />
     </section>
   );
 };
