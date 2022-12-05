eff63be5d466fbb2e525682bddb53c292db174cf
dejanV91 <dejanvukoicic91@gmail.com>
2022-12-05 10:24:13 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { useState } from "react";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
 import Error from "./pages/Error";
 import SharedLayout from "./pages/SharedLayout";
 import SingleProduct from "./pages/SingleProduct";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <BrowserRouter>
       <Routes>
@@ -17,6 +20,8 @@ function App() {
           <Route path="products" element={<Products />} />
           <Route path="products/:productId" element={<SingleProduct />} />
           <Route path="*" element={<Error />} />
+          <Route path="login" element={<Login setUser={setUser} />}></Route>
+          <Route path="dashboard" element={<Dashboard user={user} />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
